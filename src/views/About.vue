03f048170878e4fcd116898930f57d9241d10866
李祖明 <solace.lee@outlook.com>
2020-09-12 22:59:19 +0800

    神经算法

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,200 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+
+<script>
+import * as tf from '@tensorflow/tfjs'
+import * as tfvis from '@tensorflow/tfjs-vis'
+export default {
+  name: 'About',
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      console.log(tf, tfvis);
+      this.run()
+    },
+
+    createModel() {
+      // 定义模型架构
+      // Create a sequential model 实例化模型
+      const model = tf.sequential();
+      
+      // Add a single input layer 添加图层
+      /*
+      这将为我们的网络添加一个输入层，该输入层会自动连接到具有一个隐藏单元的“密集”层。
+      dense图层是一种图层类型，将其输入乘以一个矩阵（称为weights），然后将一个数字（称为bias）相加。
+      由于这是网络的第一层，因此我们需要定义inputShape。
+      这inputShape是[1]因为我们有1数字作为输入（给定汽车的马力）。
+      units 设置权重矩阵在图层中的大小。
+      通过在此处将其设置为1，我们可以说数据的每个输入要素的权重为1。
+      注意：默认情况下，密集层带有一个偏差项，因此我们不需要将useBias设置为true，我们将在以后的调用中省略tf.layers.dense
+      */
+      model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'sigmoid'}));
+      // model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+      
+      // Add an output layer
+      model.add(tf.layers.dense({units: 1, useBias: true}));
+      // model.add(tf.layers.dense({units: 50, activation: 'sigmoid'}));
+
+      return model;
+    },
+
+    async run() {
+      // Load and plot the original input data that we are going to train on.
+      const data = await this.getData();
+      const values = data.map(d => ({
+        x: d.horsepower,
+        y: d.mpg,
+      }));
+
+      tfvis.render.scatterplot(
+        {name: 'Horsepower v MPG'},
+        {values}, 
+        {
+          xLabel: 'Horsepower',
+          yLabel: 'MPG',
+          height: 300
+        }
+      );
+      const model = this.createModel();
+      tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+      // Convert the data to a form we can use for training.
+      const tensorData = this.convertToTensor(data);
+      const {inputs, labels} = tensorData;
+          
+      // Train the model
+      await this.trainModel(model, inputs, labels);
+      this.testModel(model, data, tensorData);
+      console.log('Done Training');
+    },
+    async getData () {
+      const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+      
+      const carsData = await carsDataReq.json();
+
+      const cleaned = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+      }))
+      .filter(car => (car.mpg != null && car.horsepower != null));
+      console.log(cleaned);
+      return cleaned;
+    },
+
+    /**
+     * Convert the input data to tensors that we can use for machine 
+     * learning. We will also do the important best practices of _shuffling_
+     * the data and _normalizing_ the data
+     * MPG on the y-axis.
+     */
+    convertToTensor(data) {
+      // 准备训练数据
+      // Wrapping these calculations in a tidy will dispose any 
+      // intermediate tensors.
+      
+      return tf.tidy(() => {
+        // Step 1. Shuffle the data    
+        tf.util.shuffle(data);
+
+        // Step 2. Convert data to Tensor
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg);
+
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+        //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();  
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+          inputs: normalizedInputs,
+          labels: normalizedLabels,
+          // Return the min/max bounds so we can use them later.
+          inputMax,
+          inputMin,
+          labelMax,
+          labelMin,
+        }
+      });
+    },
+    async trainModel(model, inputs, labels) {
+      // 训练模型
+      // Prepare the model for training.  
+      model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+      });
+      
+      const batchSize = 32;
+      const epochs = 50;
+      
+      return await model.fit(inputs, labels, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+          { name: 'Training Performance' },
+          ['loss', 'mse'], 
+          { height: 200, callbacks: ['onEpochEnd'] }
+        )
+      });
+    },
+
+    testModel(model, inputData, normalizationData) {
+      // 做出预测
+      const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+      
+      // Generate predictions for a uniform range of numbers between 0 and 1;
+      // We un-normalize the data by doing the inverse of the min-max scaling 
+      // that we did earlier.
+      const [xs, preds] = tf.tidy(() => {
+        
+        const xs = tf.linspace(0, 1, 100);      
+        const preds = model.predict(xs.reshape([100, 1]));      
+        
+        const unNormXs = xs
+          .mul(inputMax.sub(inputMin))
+          .add(inputMin);
+        
+        const unNormPreds = preds
+          .mul(labelMax.sub(labelMin))
+          .add(labelMin);
+        
+        // Un-normalize the data
+        return [unNormXs.dataSync(), unNormPreds.dataSync()];
+      });
+      
+    
+      const predictedPoints = Array.from(xs).map((val, i) => {
+        return {x: val, y: preds[i]}
+      });
+      
+      const originalPoints = inputData.map(d => ({
+        x: d.horsepower, y: d.mpg,
+      }));
+      
+      
+      tfvis.render.scatterplot(
+        {name: 'Model Predictions vs Original Data'}, 
+        {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+        {
+          xLabel: 'Horsepower',
+          yLabel: 'MPG',
+          height: 300
+        }
+      );
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,167 +1,83 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{ plus10 }}
+    <div @click="addReactive">添加</div>
+    <div>{{ test2.join() }}</div>
+    <div>{{ test3.bValue.join() }}</div>
+    <div @click="changeObj">测试reactive</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { reactive, getCurrentInstance, watch, ref, computed, onMounted, toRefs } from 'vue'
 
+function haha () {
+  console.log('haha');
+}
 export default {
   name: 'Home',
-  created () {
-    this.init5()
+  setup () {
+    const { ctx } = getCurrentInstance();
+    console.log(ctx.$router.currentRoute.value.path);
+
+    //点击按钮 ref ++
+    const num = ref(1);
+    const addRef = () => {
+      num.value++;
+    };
+
+    //点击按钮 reactive ++
+    const count = reactive({ value: 1 });
+    const obj = reactive({
+      test1: '李',
+      test2: ['solace'],
+      test3: {
+        aValue: '哈哈',
+        bValue: ['打印']
+      }
+    })
+
+    const changeObj = () => {
+      obj.test3.bValue.push('打印出来了吗')
+      obj.test2.push('lee')
+      haha()
+    }
+    const addReactive = () => {
+      count.value++;
+    };
+    watch(count, (newVal, oldVal, clean) => {
+      console.log(newVal.value + "," + oldVal.value);
+      clean(() => console.log("Clean"));
+    });
+    //计算×10
+    const plus10 = computed(() => count.value * 10);
+
+    //获取state.status
+    const status = computed(() => ctx.$store.state.status);
+    const setStatus = () => {
+      ctx.$store.commit("setStatus", status.value);
+      console.log(ctx.$store.state.status);
+    };
+    onMounted(() => {console.log('onmounted');
+    })
+    return {
+      num,
+      count,
+      addRef,
+      addReactive,
+      plus10,
+      status,
+      setStatus,
+      changeObj,
+      ...toRefs(obj)
+    };
+    
   },
   methods: {
     init () {
-      let i = 0;
-      function func() {
-        const t = 1;
-        i += t;
-        return i;
-      }
-      console.log(func()); // 2
 
-      function func() {
-        const t = 2;
-        i += t;
-        return i;
-      }
-
-      console.log(func());// 4
-    },
-
-    init1 () {
-      var t = 'A';
-      function func() {
-        var t = 'B';
-        function f() {
-          return t;
-        }
-        return f()
-      }
-      console.log(func()); // B
-    },
-
-    init2 () {
-      var t = 'A';
-      function func() {
-        var t = 'B';
-        function f() {
-          return t;
-        }
-        return f
-      }
-      console.log(func()()); // B
-    },
-
-    init3 () {
-      function f0(){
-        try {
-          console.log(this.a, this.b);
-        } catch (error) {
-          console.log('f0', error);
-        }
-      }
-      var a = 1;
-      let b = 2;
-      
-      let obj = {
-        a: 3,
-        b: 4,
-        f1: () => {
-          console.log(this.a);
-        },
-        f2: function () {
-          try {
-            console.log(this.a);
-          } catch (error) {
-            console.log('f2', error);
-          }
-        },
-        f3() {
-          try {
-            console.log(this.a);
-          } catch (error) {
-            console.log('f3', error);
-          }
-        },
-        f4: f0
-      };
-      let f5 = obj.f1;
-      let f6 = obj.f2;
-      let f7 = obj.f3;
-      let f8 = obj.f4;
-      let f9 = obj.f1.bind(obj);
-      let f10 = obj.f2.bind(obj);
-      let f11 = obj.f1.bind(this);
-      let f12 = obj.f2.bind(this);
-
-      f0(); // undefind
-      obj.f1(); // undefind
-      obj.f2(); // 3
-      obj.f3(); // 3
-      obj.f4(); // 3 4
-      f5(); // 4
-      f6(); // undefind
-      f7(); // undefind
-      f8(); // undefind
-      f9(); // undefind
-      f10(); // 3
-      f11(); // undefind
-      f12(); // undefind
-
-      // 浏览器环境下执行结果
-      // f0(); // 1 undefind
-      // obj.f1(); // 1
-      // obj.f2(); // 3
-      // obj.f3(); // 3
-      // obj.f4(); // 3 4
-      // f5(); // 1
-      // f6(); // 1
-      // f7(); // 1
-      // f8(); // 1 undefind
-      // f9(); // 1
-      // f10(); // 3
-      // f11(); // 1
-      // f12(); // 1
-    },
-
-    init4 () {
-      for (let i = 0; i < 3; i++) {
-        setTimeout(function () {
-          console.log(i); // 0 1 2
-        }, 0)
-      }
-    },
-
-    init5 () {
-      console.log(1);
-      new Promise(resolve => {
-        console.log(2);
-        setTimeout(() => {
-          console.log(3);
-        }, 10)
-        resolve()
-        console.log(4);
-      }).then(() => {
-        console.log(5);
-      })
-
-      setTimeout(() => {
-        console.log(6);
-        Promise.resolve().then(() => {
-          console.log(7);
-        })
-        console.log(8);
-      })
-
-      Promise.resolve().then(() => {
-        console.log(9);
-      })
-
-      console.log(10);
-      // 1 2 4 10 5 9 6 8 7 3
     }
   }
 }
